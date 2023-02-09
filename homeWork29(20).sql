d687aa0625fd0ba011fd872bfee36dfe3ad97094
reteruvsky <thebingolo@gmail.com>
2023-02-09 20:00:10 +0200

    changed selects

--- homeWork29(20).sql
+++ homeWork29(20).sql
@@ -1,23 +1,32 @@
-CREATE DATABASE Person;
-USE Person;
-CREATE TABLE users ( 
+CREATE TABLE person (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(30), 
     lastName VARCHAR(30),
     age INTEGER,
     city VARCHAR(30)
-);
-
-RENAME TABLE users TO Person;
-
+    );
+    
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Corey', 'Taylor', 53, 'Des Moines');
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Shawn', 'Crahan', 49, 'Houston');
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Craig', 'Jones', 50, 'New York');
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Mick', 'Thomson', 49, 'Atlanta');
-INSERT INTO person (firstName, lastName, age, city) VALUES ('Sid', 'Wilso', 46, 'Boston');
+INSERT INTO person (firstName, lastName, age, city) VALUES ('Sid', 'Wilson', 46, 'Boston');
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Jim', 'Root', 51, 'Las Vegas');
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Alessandro', 'Venturella', 44, 'London');
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Jay', 'Weinberg', 32, 'New Jersey');
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Chester ', 'Bennington', 46, 'Phoenix');
 INSERT INTO person (firstName, lastName, age, city) VALUES ('Michael', 'Shinoda', 45 , 'Los Angeles');
 
-SELECT city, age, firstName from person
+SELECT firstName, lastName from person;
+SELECT firstName, age from person;
+SELECT firstName, lastName, age, city from person;
+
+SELECT * from person
+ORDER BY city;
+
+SELECT * from person
+ORDER BY age;
+
+SELECT * from person
+ORDER BY firstName;
+    
